f279849d16d40500832193ab3d86afc0d7161d40
sidalihmdn <93407317+sidalihmdn@users.noreply.github.com>
2021-10-29 23:08:53 +0200

    Add files via upload

---
+++ my.py
@@ -0,0 +1,23 @@
+from flask import Flask , render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello world'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/blog/<string:blog_id>')
+def blogpost(blog_id):
+    return 'This is the blog number ' + blog_id
+
+if __name__ == '__main__':
+    app.run()
